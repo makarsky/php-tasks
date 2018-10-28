3216bc81a5b2dccb4af951c6241d467cd9f003e9
Ihar Makarski <22708426+makarsky@users.noreply.github.com>
2018-10-28 23:58:29 +0300

    Update Team.php

--- php-oop-task/Itransition/Entities/Team.php
+++ php-oop-task/Itransition/Entities/Team.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Itransition\Entities;
 
 use Itransition\Interfaces\Payable;
@@ -10,22 +9,24 @@ class Team implements Payable
     public $title;
     private $team = [];
 
-    function __construct($title)
+    public function __construct($title)
     {
         $this->title = $title;
     }
 
-    function addEmployee(Employee $employee)
+    public function addEmployee(Employee $employee)
     {
         $this->team[] = $employee;
     }
 
-    function getSalary(): float
+    public function getSalary(): float
     {
         $teamSalary = 0;
+        
         foreach ($this->team as $employee) {
             $teamSalary += $employee->getSalary();
         }
+        
         return $teamSalary;
     }
 }
